ea7473ff5800dd2d215322c47bedb5ae8f2d8f6d
mogg22 <ahrthdus1@gmail.com>
2023-11-06 19:36:42 +0900

    [1106]main페이지 now페이지로 변경

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Login from './pages/Login';
-import Main from './pages/Main';
+import Now from './pages/Now';
 import Feed from './pages/Feed';
 import Search from './pages/Search';
 import Myshelf from './pages/Myshelf';
@@ -12,11 +12,12 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path='/' element={<Login/>}/>
-          <Route exact path='/main' element={<Main/>}/>
+          <Route exact path='/now' element={<Now/>}/>
           <Route exact path='/feed' element={<Feed/>}/>
           <Route exact path='/search' element={<Search/>}/>
           <Route exact path='/myshelf' element={<Myshelf/>}/>
           <Route exact path='/management' element={<Management/>}/>
+        
         </Routes>
       </div>
     </BrowserRouter>
