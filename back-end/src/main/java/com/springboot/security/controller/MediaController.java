abe17670b19b6c194a23b03d26c424ce2b7fd79f
7beunseo <kimes0403@gmail.com>
2023-11-06 11:12:26 +0900

    [1106] 미디어 여러개 업로드, 미디어 확인 추가

--- back-end/src/main/java/com/springboot/security/controller/MediaController.java
+++ back-end/src/main/java/com/springboot/security/controller/MediaController.java
@@ -1,18 +1,24 @@
 package com.springboot.security.controller;
 
+import com.springboot.security.data.dto.MediaDescriptorDto;
+import com.springboot.security.service.MediaService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("media")
@@ -38,6 +44,46 @@ public class MediaController {
             e.printStackTrace();
             logger.info("파일삭제됨");
         }
+
+    }
+    private final MediaService mediaService;
+    public MediaController(MediaService mediaService){
+        this.mediaService=mediaService;
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @PostMapping("upload")
+    public ResponseEntity<MediaDescriptorDto> uploadMedia2(
+            @RequestParam("file") MultipartFile file
+    ){
+        MediaDescriptorDto descriptorDto = this.mediaService.saveFile(file);
+        return ResponseEntity
+                .status(descriptorDto.getStatus())
+                .body(descriptorDto);
+    }
+
+    @PostMapping("upload-bulk")
+    public ResponseEntity<Collection<MediaDescriptorDto>> uploadMediaBulk(
+            @RequestParam("file") MultipartFile[] files
+    ){
+        return ResponseEntity
+                .status(HttpStatus.MULTI_STATUS)
+                .body(this.mediaService.saveFileBulk(files));
+    }
+
+    @GetMapping("**")
+    public ResponseEntity<byte[]> staticLikeEndpoint(
+            HttpServletRequest request
+    ){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+        return new ResponseEntity<>(
+                this.mediaService.getFileAsBytes(request.getRequestURI().split("media")[1]),
+                headers,
+                HttpStatus.OK
+        );
 
     }
 }
